122024ee7cf9408aa7973afc6bc4cceedcbed708
XEON <hiepphamlc02@gmail.com>
2025-08-22 12:20:55 +0700

    First commit

--- core/src/main/java/io/github/some_example_name/Bird.java
+++ core/src/main/java/io/github/some_example_name/Bird.java
@@ -1,48 +1,111 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 public class Bird {
+    private TextureRegion[] frames;
+    private Animation<TextureRegion> animation;
     private Texture texture;
     private float x, y;
+    private float width, height;
+    private float stateTime;
     private float velocityY;
-    private final float GRAVITY = -500; // pixel/s²
+    private float gravity; // pixel/s²
+    private Rectangle hitbox;
 
     public Bird() {
         texture = new Texture("objects/yellowbird-downflap.png");
-        x = 100;
-        y = 300;
         velocityY = 0;
+
+        Texture textureDown = new Texture("objects/yellowbird-downflap.png");
+        Texture textureMid = new Texture("objects/yellowbird-midflap.png");
+        Texture textureUp = new Texture("objects/yellowbird-upflap.png");
+
+        frames = new TextureRegion[3];
+        frames[0] = new TextureRegion(textureDown);
+        frames[1] = new TextureRegion(textureMid);
+        frames[2] = new TextureRegion(textureUp);
+
+        animation = new Animation<TextureRegion>(0.275f, new Array<TextureRegion>(frames));
+        stateTime = 0f;
+
     }
 
     public void update(float delta) {
-//        velocityY += GRAVITY * delta;
-//        y += velocityY * delta;
-//
-//        // chạm đất
-//        if (y < 0) {
-//            y = 0;
-//            velocityY = 0;
-//        }
+        stateTime += delta;
+
+        velocityY += gravity * delta;
+        y += velocityY * delta;
+
+        // chạm đất
+        if (y < 0) {
+            y = 0;
+            velocityY = 0;
+        }
+
+    }
+
+    public void setHitboxSize(float width, float height) {
+        this.width = width;
+        this.height = height;
+        hitbox = new Rectangle(x, y, this.width, this.height);
+        hitbox.setPosition(x, y);
     }
 
     public void jump() {
-        velocityY = 300; // nhảy lên
+        velocityY = 1000; // nhảy lên
+        stateTime = 0f;
     }
 
     public void render(SpriteBatch batch) {
-        float scale = 1f; // thu nhỏ còn 50%
-        batch.draw(texture, x, y, 34 * scale, 24 * scale);
+        float scale = 5f;
+        TextureRegion currentFrame = animation.getKeyFrame(stateTime, true); // Lặp animation
+        batch.draw(currentFrame, x, y, 34 * scale, 24 * scale);
     }
 
     public void dispose() {
         texture.dispose();
     }
 
-    // Getter để check va chạm
-    public float getX() { return x; }
-    public float getY() { return y; }
-    public float getWidth() { return 34; }
-    public float getHeight() { return 24; }
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setGravity(float gravity) {
+        this.gravity = gravity;
+    }
+
+    public void setVelocityY(float velocityY) {
+        this.velocityY = velocityY;
+
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public float getWidth() {
+        return 34;
+    }
+
+    public float getHeight() {
+        return 24;
+    }
 }
--- core/src/main/java/io/github/some_example_name/GameScreen.java
+++ core/src/main/java/io/github/some_example_name/GameScreen.java
@@ -1,14 +1,22 @@
 package io.github.some_example_name;
 
+import static java.rmi.server.LogStream.log;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
@@ -28,48 +36,135 @@ public class GameScreen implements Screen {
     private int backgroundOffset;
 
     //World parameters
-    private final int WORLD_WIDTH = 800;
-    private final int WORLD_HEIGHT = 480;
+    private int WORLD_WIDTH;
+    private int WORLD_HEIGHT;
+
+    //Other parameters
+    private int scrollSpeed;
+    private Stage stage;
+    private BitmapFont font;
+    private Label.LabelStyle style;
+    private Label logLabel;
 
     //Objects
     private Bird bird;
-
-    GameScreen() {
-        bird = new Bird();
-
-        camera = new OrthographicCamera();
-        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
-
-        background = new Texture("objects/background-day.png");
-        backgroundOffset = 0;
-
-        batch = new SpriteBatch();
-
-
-    }
+    private ShapeRenderer shapeRenderer;
 
     @Override
     public void show() {
+        SetWorldSize();
+        SetBirdPosition();
+        setView();
+        setBackground();
+
+        shapeRenderer = new ShapeRenderer();
+        scrollSpeed = 300;
+
+        stage = new Stage(new ScreenViewport());
+        font = new BitmapFont();
+        font.getData().setScale(5f);
+        style = new Label.LabelStyle(font, Color.WHITE);
+        logLabel = new Label("Debug log...", style);
 
     }
 
     @Override
     public void render(float delta) {
-        backgroundOffset += 100 * delta; // tốc độ (pixel/giây)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        scrollBackround(delta);
+
+        batch.begin();
+        drawBackground();
+
+        bird.update(delta);
+        bird.render(batch);
+
+        touchDown();
+        if (bird.getY() > WORLD_HEIGHT - (24 * 5)) {
+            bird.setY(WORLD_HEIGHT - (24 * 5));
+            bird.setVelocityY(0);
+            System.out.println("Đu đỉnh rồi huhu ");
+        }
+
+        batch.end();
+
+        drawHitbox();
+
+        drawLog(delta);
+
+    }
+
+    public void SetWorldSize() {
+        WORLD_WIDTH = Gdx.graphics.getWidth();
+        WORLD_HEIGHT = Gdx.graphics.getHeight();
+    }
+
+    public void SetBirdPosition() {
+        bird = new Bird();
+        bird.setX(WORLD_WIDTH / 2 - (17 * 5));
+        bird.setY(WORLD_HEIGHT / 2);
+        bird.setGravity(-3000);
+
+
+    }
+
+    public void setView() {
+        camera = new OrthographicCamera();
+        viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+    }
+
+    public void setBackground() {
+        background = new Texture("objects/background-day.png");
+        backgroundOffset = 0;
+
+        batch = new SpriteBatch();
+    }
+
+    public void scrollBackround(float delta) {
+        backgroundOffset += scrollSpeed * delta; // tốc độ (pixel/giây)
         if (backgroundOffset > WORLD_WIDTH) {
             backgroundOffset = 0;
         }
+    }
 
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        bird.update(delta);
-
-        // Vẽ 2 cái background kế nhau theo trục X
+    public void drawBackground() {
+        // Vẽ 2 cái background liền nhau theo trục X
         batch.draw(background, -backgroundOffset, 0, WORLD_WIDTH, WORLD_HEIGHT);
         batch.draw(background, -backgroundOffset + WORLD_WIDTH, 0, WORLD_WIDTH, WORLD_HEIGHT);
+    }
 
-        bird.render(batch);
-        batch.end();
+    public void drawHitbox() {
+        bird.setHitboxSize(34 * 5, 24 * 5);
+        // vẽ hitbox debug
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.RED);
+
+        // vẽ hitbox của chim
+        shapeRenderer.rect(
+            bird.getHitbox().x,
+            bird.getHitbox().y,
+            bird.getHitbox().width,
+            bird.getHitbox().height
+        );
+
+        shapeRenderer.end();
+    }
+    public void drawLog(float delta){
+        logLabel.setPosition(bird.getX() + (34 * 5), bird.getY()); // tọa độ trên màn hình
+        stage.addActor(logLabel);
+        stage.act(delta);
+        logLabel.setText((int)bird.getY());
+        stage.draw();
+    }
+    public void touchDown() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                bird.jump();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -80,7 +175,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        background.dispose();
+        shapeRenderer.dispose();
     }
 
 
